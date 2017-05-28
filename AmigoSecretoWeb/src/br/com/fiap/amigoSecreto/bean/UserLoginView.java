fbbfb90723018d02aff8111c354daeea8a8f1875
victoriav <ventura.victoria@forceconsulting.com.br>
2017-05-28 04:11:49 -0300

    Criação da página de login, autenticacao, session scope e página de menu

--- AmigoSecretoWeb/src/br/com/fiap/amigoSecreto/bean/UserLoginView.java
+++ AmigoSecretoWeb/src/br/com/fiap/amigoSecreto/bean/UserLoginView.java
@@ -2,84 +2,47 @@ package br.com.fiap.amigoSecreto.bean;
  
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
- 
-import org.primefaces.context.RequestContext;
 
 import br.com.fiap.amigoSecreto.dao.UsuarioDAO;
 import br.com.fiap.amigoSecreto.entity.Usuario;
  
-@ManagedBean
+@ManagedBean(name="loginUsuarios")
+@SessionScoped
 public class UserLoginView {
-     
-    private String email;
-     
-    private String senha;
+
+	private UsuarioDAO dao = new UsuarioDAO();
+	
+	public UserLoginView(){
+		usuario = new Usuario();
+	}
+ 
+	private Usuario usuario;
     
-    private UsuarioDAO dao = new UsuarioDAO();
- 
-    public String getUsername() {
-        return email;
-    }
- 
-    public void setUsername(String username) {
-        this.email = username;
-    }
- 
-    public String getPassword() {
-        return senha;
-    }
- 
-    public void setPassword(String password) {
-        this.senha = password;
-    }
+    public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
    
-    public String login() {
-        RequestContext context = RequestContext.getCurrentInstance();
-        FacesMessage message = null;
-        boolean loggedIn = false;
-        Usuario usuario = new Usuario();
-        String retorno = ""; 
-         
-        if((email != null && !email.equals("")) && (senha != null && !senha.equals(""))) {
-            loggedIn = true;
-            
-           usuario = dao.buscaUsuarioLogin(email, senha);
-           
-           if(usuario != null){
-        	   retorno = "perfil.xhtml";
-           }
-        } else {
-            loggedIn = false;
-            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
-            retorno = "";
-            FacesContext.getCurrentInstance().addMessage(null, message);
-            context.addCallbackParam("loggedIn", loggedIn);
-            
-        }
-         
-       
-       
+	public String validarUsuario() throws Exception{
+   		
+		if(dao.buscaUsuarioLogin(usuario.getEmail(), usuario.getSenha()) != null){
+			return "menu.xhtml";
+		} else {
+			FacesContext context = FacesContext.getCurrentInstance();
+			FacesMessage msg = new FacesMessage();
+			msg.setSummary("Erro Login: ");
+			msg.setDetail("Usuário ou senha inválidos");
+			msg.setSeverity(FacesMessage.SEVERITY_FATAL);
+			
+			context.addMessage(null, msg);
+			return "login.xhtml";
+		}    
         
-        return retorno;
     }
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}   
-    
     
 }
